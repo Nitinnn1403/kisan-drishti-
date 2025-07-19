88c5747f51543cb605bfd94ad98688125788b307
Nitinnn1403 <heynitin06@gmail.com>
2025-07-19 19:25:00 +0530

    fix : fixed login

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 allowed_origins = [
-    "https://kisan-drishti.vercel.app",
+    "https://kisandrishti.vercel.app",
     "http://127.0.0.1:5500",
     "http://localhost:5500"
 ]
